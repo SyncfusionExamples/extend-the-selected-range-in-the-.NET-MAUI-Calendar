c7d1fa7c15f17e83afcd3e5e900aef025c88b1ee
VishalOmprasad <vishal.omprasad@syncfusion.com>
2023-01-31 23:38:19 +0530

    New KB sample added

---
+++ SelectedRangeExtension/SelectedRangeExtension/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SelectedRangeExtension;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
